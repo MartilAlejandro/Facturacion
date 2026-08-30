7787b358eb8b8712ac6f57ae8ff091e4da11622d
MartilAlejandro <martil_alejandro@unitec.edu>
2026-08-30 13:21:01 -0600

    fact

--- sistemafacturacion/src/sistemafacturacion/Sistemafacturacion.java
+++ sistemafacturacion/src/sistemafacturacion/Sistemafacturacion.java
@@ -19,6 +19,7 @@ public class Sistemafacturacion {
         // TODO code application logic here
         Scanner entrada = new Scanner(System.in);
         int hola=0;
+        int tu =0;
         
     }
     
