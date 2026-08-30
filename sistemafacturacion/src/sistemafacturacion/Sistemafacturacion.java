0c81206d635468457b5a0a6cdef41159f03348b2
MartilAlejandro <martil_alejandro@unitec.edu>
2026-08-30 14:29:44 -0600

    Delete sistemafacturacion directory

--- sistemafacturacion/src/sistemafacturacion/Sistemafacturacion.java
+++
@@ -1,9 +0,0 @@
-    public static void main(String[] args) {
-        // TODO code application logic here
-        Scanner entrada = new Scanner(System.in);
-
-        System.out.println("sistemafacturacion");
-        System.out.println("hola");
-        System.out.println("Prueba de merge Domingo");
-    }// FIN DE MAIN
-
