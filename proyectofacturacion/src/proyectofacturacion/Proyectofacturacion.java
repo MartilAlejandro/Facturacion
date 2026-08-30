0ca27d4eaf56ee4cc078bbc1c4dd60c30caa5893
MartilAlejandro <martil_alejandro@unitec.edu>
2026-08-30 14:48:25 -0600

    yes

--- proyectofacturacion/src/proyectofacturacion/Proyectofacturacion.java
+++ proyectofacturacion/src/proyectofacturacion/Proyectofacturacion.java
@@ -18,7 +18,8 @@ public class Proyectofacturacion {
     public static void main(String[] args) {
         // TODO code application logic here
         
-        Scanner entrada = new Scanner(System.in);
+       
+         Scanner entrada = new Scanner(System.in);
     }
     
 }
