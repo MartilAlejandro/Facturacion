ea527121bdebcee9c7797941633003fe3ae9b77b
dbueno008 <danielbueno@unitec.edu>
2026-09-19 18:28:09 -0600

    Create .gitignore

--- proyectofacturacion/src/proyectofacturacion/Producto.java
+++ proyectofacturacion/src/proyectofacturacion/Producto.java
@@ -10,7 +10,7 @@ public class Producto {
     private String nombreProd;
     private int codigo;
     private char categoria;
-    private boolean impuesto;
+    boolean impuesto;
     private double precio;
     private int cantidad;
     private double peso;
